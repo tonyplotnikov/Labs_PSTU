26706d78ed3be1151e12bbb263f2bcfff0ca2b9b
tonyplotnikov <63773654+tonyplotnikov@users.noreply.github.com>
2020-04-22 08:47:09 +0500

    Add files via upload

---
+++ Lab12spiskistekiocheredi_AntonPlotnikov/laba12.cpp
@@ -0,0 +1,16 @@
+﻿// Лабораторная работа №12. Плотников Антон. ИВТ-19-1Б.
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Header1.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int g;
+	cout << "1.Однонаправленный список\n";
+	cout << "2.Двунаправленный список\n";
+	cout << "3.Стек\n";
+	cout << "4.Очередь\n";
+	cin >> g;
+	variant(g);
+}
