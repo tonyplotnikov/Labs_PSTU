31550b66121a07c481d7f69826d3ac1a781fe84e
tonyplotnikov <63773654+tonyplotnikov@users.noreply.github.com>
2020-04-25 23:15:15 +0500

    Add files via upload

---
+++ 19 Бинарные деревья/Lab 19.cpp
@@ -0,0 +1,85 @@
+﻿// Плотников Антон. Лабораторная работа №19 / Бинарное дерево.
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <cmath>
+using namespace std;
+int tabs = 0;
+
+struct Tree
+{
+    char data;
+    Tree* left;
+    Tree* right;
+};
+
+void print_tree(Tree*& root)
+{
+
+    if (root == NULL)
+    {
+        return;
+    }
+    else
+    {
+        tabs++;
+        print_tree(root->left);
+
+        for (int i = 0; i < tabs; i++)
+        {
+            cout << " ";
+        }
+        cout << root->data << endl;
+
+        print_tree(root->right);
+
+        tabs--;
+    }
+}
+
+void create_tree(Tree*& root)
+{
+
+    if (root == NULL)
+    {
+        root = new Tree;
+        root->data = (char)rand();
+        root->left = NULL;
+        root->right = NULL;
+        return;
+    }
+    else
+    {
+        create_tree(root->left);
+        create_tree(root->right);
+    }
+
+}
+
+int tree_height(const Tree* p)
+{
+    int l, r, h = 0;
+    if (p != NULL)
+    {
+        l = tree_height(p->left);
+        r = tree_height(p->right);
+        h = ((l > r) ? l : r) + 1;
+    }
+    return h;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+    Tree* root = NULL;
+    int n = rand() % 10;
+
+    for (int i = 0; i < n; i++)
+    {
+        create_tree(root);
+    }
+
+    print_tree(root);
+    cout << "Высота дерева - " << tree_height(root);
+}
