9854dd778d32ebc1200ea689c1c615ea9d14b089
tonyplotnikov <63773654+tonyplotnikov@users.noreply.github.com>
2020-05-04 11:06:17 +0300

    Add files via upload

---
+++ Lab 13 Рекурсия/Рекурсивные функции.cpp
@@ -0,0 +1,24 @@
+﻿//Плотников Антон, Лабораторная работа №13. Рекурсия.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, с = 1;
+    cin >> a >> b;
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a -= b;
+
+        }
+        else
+        {
+            b -= a;
+        }
+        ++с;
+    }
+    cout << с;
+}
