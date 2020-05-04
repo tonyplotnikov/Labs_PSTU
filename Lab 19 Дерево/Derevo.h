9062610b291e8c4a99c9b76bc61d90e8a654c315
tonyplotnikov <63773654+tonyplotnikov@users.noreply.github.com>
2020-05-04 11:18:19 +0300

    Add files via upload

---
+++ Lab 19 Дерево/Derevo.h
@@ -0,0 +1,102 @@
+﻿//Лабораторная работа №19. Бинарное дерево. Плотников Антон.
+//1. Сформировать сбалансированное бинарное дерево.
+//2. Распечатать полученное дерево.
+//3. Найти высоту дерева.
+//4. Преобразовать сбалансированное дерево в дерево поиска.
+//5. Распечатать полученное дерево.
+#pragma once
+using namespace std;
+struct point
+{
+	char data;
+	point* left;
+	point* right;
+};
+point* first(char d)
+{
+	point* p = new point;
+	p->data = d;
+	p->left = 0;
+	p->right = 0;
+	return p;
+}
+point* Add(point* p, char d)
+{
+	if (p == NULL)
+	{
+		p = new point;
+		p->data = d;
+		p->left = NULL;
+		p->right = NULL;
+	}
+	else if (d < p->data)
+	{
+		p->left = Add(p->left, d);
+	}
+	else
+	{
+		p->right = Add(p->right, d);
+	}
+	return p;
+}
+point* Tree(int n, point* p)
+{
+	point* r;
+	int nl, nr;
+	if (n == 0) { p = NULL; return p; }
+	nl = n / 2;
+	nr = n - nl - 1;
+	r = new point;
+	cout << "Ââåäèòå ýëåìåíò äåðåâà: ";
+	cin >> r->data;
+	r->left = Tree(nl, r->left);
+	r->right = Tree(nr, r->right);
+	p = r;
+	return p;
+}
+void addToArray(point* p, char* a, size_t& i)
+{
+	a[i++] = p->data;
+	if (p->left)
+	{
+		addToArray(p->left, a, i);
+	}
+	if (p->right)
+	{
+		addToArray(p->right, a, i);
+	}
+}
+int High(point* p)
+{
+	int l, r, h = 0;
+	if (p)
+	{
+		l = High(p->left);
+		r = High(p->right);
+		h = ((l > r) ? l : r) + 1;
+	}
+	return h;
+}
+void DestroyTree(point* p)
+{
+	if (p != NULL)
+	{
+		DestroyTree(p->left);
+		DestroyTree(p->right);
+		delete(p);
+	}
+}
+void Run(point* p, int n, int q)
+{
+	if (p)
+	{
+		Run(p->right, n + 1, q);
+		for (int i = q; i < n; i++)
+		{
+			cout << "      ";
+		}
+		cout << "(" << p->data << ")" << "\n";
+		Run(p->left, n + 1, q);
+	}
+	cout << "\n";
+}
---
+++ Lab 19 Дерево/lab19.cpp
@@ -0,0 +1,56 @@
+﻿//Лабораторная работа №19. Бинарное дерево. Плотников Антон.
+#include <iostream>
+#include "Derevo.h"
+int main()
+{
+	setlocale(LC_ALL,"rus");
+	int n, g, h, q;
+	cout << "Введите количество символов: ";
+	cin >> n;
+	q = n;
+	char* a = new char[n];
+	point* p = new point;
+	do
+	{
+		cout << "1. Сформировать сбалансированное бинарное дерево\n";
+		cout << "2. Распечатать полученное дерево\n";
+		cout << "3. Найти высоту дерева\n";
+		cout << "4. Преобразовать сбалансированное дерево в дерево поиска\n";
+		cout << "5. Выход\n";
+		cin >> g;
+		switch (g)
+		{
+		case 1:
+		{
+			p = Tree(n, p);
+			break;
+		}
+		case 2:
+		{
+			Run(p, n, q);
+			break;
+		}
+		case 3:
+		{
+			h = High(p);
+			cout << "Высота дерева: " << h << endl;
+			break;
+		}
+		case 4:
+		{
+			char d;
+			size_t i = 0;
+			addToArray(p, a, i);
+			DestroyTree(p);
+			d = a[0];
+			first(d);
+			for (int i = 1; i < n; i++)
+			{
+				d = a[i];
+				Add(p, d);
+			}
+			break;
+		}
+		}
+	} while (g != 5);
+}
