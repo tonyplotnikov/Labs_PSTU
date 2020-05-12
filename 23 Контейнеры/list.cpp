b112aa9e351ccf53d0f5ebf1ee0e3980015b3df5
tonyplotnikov <63773654+tonyplotnikov@users.noreply.github.com>
2020-05-12 15:15:54 +0500

    Add files via upload

---
+++ 23 Контейнеры/list.cpp
@@ -0,0 +1,72 @@
+﻿// Плотников Антон, лабораторная работа №23. АТД. Контейнеры
+// Класс контейнер-список с ключевыми значениями. Тип: int.
+// Реализовать операции :
+// [] – доступ по индексу;
+// () – определение размера списка;
+// ++ - перейти к следующему элементу (с помощью класса - итератора);
+// +число – добавить константу к каждому элементу списка.
+
+#include "List.h"
+List::List(int s, int k)
+{
+	size = s;
+	data = new int[size];
+	for (int i = 0; i < size; i++)
+		data[i] = k;
+	beg.elem = &data[0];
+	end.elem = &data[size];
+}
+List::List(const List& a)
+{
+	size = a.size;
+	data = new int[size];
+	for (int i = 0; i < size; i++)
+		data[i] = a.data[i];
+	beg = a.beg;
+	end = a.end;
+}
+List::~List()
+{
+	delete[]data;
+	data = 0;
+}
+List& List::operator=(const List& a)
+{
+	if (this == &a)return *this;
+	size = a.size;
+	if (data != 0) delete[]data;
+	data = new int[size];
+	for (int i = 0; i < size; i++)
+		data[i] = a.data[i];
+	beg = a.beg;
+	end = a.end;
+	return *this;
+}
+int& List::operator[](int index)
+{
+	if (index < size) return data[index];
+	else cout << "\nError! Index>size";
+}
+List List::operator+(const int k)//+k
+{
+	List temp(size);
+	for (int i = 0; i < size; ++i)
+		temp.data[i] += data[i] + k;
+	return temp;
+}
+int List::operator ()()
+{
+	return size;
+}
+ostream& operator<<(ostream& out, const List& a)
+{
+	for (int i = 0; i < a.size; ++i)
+		out << a.data[i] << " ";
+	return out;
+}
+istream& operator>>(istream& in, List& a)
+{
+	for (int i = 0; i < a.size; ++i)
+		in >> a.data[i];
+	return in;
+}
---
+++ 23 Контейнеры/list.h
@@ -0,0 +1,45 @@
+﻿// Плотников Антон, лабораторная работа №23. АТД. Контейнеры
+// Класс контейнер-список с ключевыми значениями. Тип: int.
+// Реализовать операции :
+// [] – доступ по индексу;
+// () – определение размера списка;
+// ++ - перейти к следующему элементу (с помощью класса - итератора);
+// +число – добавить константу к каждому элементу списка.
+
+#pragma once
+#include <iostream>
+using namespace std;
+class Iterator
+{
+	friend class List;
+public:
+	Iterator() { elem = 0; }
+	Iterator(const Iterator& it) { elem = it.elem; }
+	bool operator==(const Iterator& it) { return elem == it.elem; }
+	bool operator!=(const Iterator& it) { return elem != it.elem; }
+	void operator++() { ++elem; }
+	void operator--() { --elem; }
+	int& operator *() const { return*elem; }
+private:
+	int* elem;
+};
+class List
+{
+public:
+	List(int s, int k = 0);
+	List(const List& a);
+	~List();
+	List& operator=(const List& a);
+	int& operator[](int index);
+	List operator+(const int k);
+	int operator()();
+	friend ostream& operator<<(ostream& out, const List& a);
+	friend istream& operator>>(istream& in, List& a);
+	Iterator first() { return beg; }
+	Iterator last() { return end; }
+private:
+	int size;
+	int* data;
+	Iterator beg;
+	Iterator end;
+};
