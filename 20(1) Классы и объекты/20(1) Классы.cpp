269dbe08770f7caac927ca56e0dcf0965f903f8d
tonyplotnikov <63773654+tonyplotnikov@users.noreply.github.com>
2020-05-12 14:24:53 +0500

    Add files via upload

---
+++ 20(1) Классы и объекты/20(1) Классы.cpp
@@ -0,0 +1,35 @@
+﻿//Антон Плотников, лабораторная работа №20(1). Классы и Объекты. Инкапсуляция, конструкторы.
+#include <iostream>
+#include "fraction.h"
+using namespace std;
+void fraction::init(int F, double S)
+{
+	first = F;
+	second = S;
+}
+void fraction::show()
+{
+	cout << "\nfirst = " << first;
+	cout << "\nsecond = " << second;
+	cout << endl;
+}
+void fraction::read()
+{
+	cout << "\n Время: ";
+	cin >> first;
+	cout << "\n Цена за одну единицу времени: ";
+	cin >> second;
+}
+double fraction::cost()
+{
+	return first * second;
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	fraction A;
+	A.read();
+	A.show();
+	A.cost();
+	cout << "Стоимость разговора: " << A.cost() << endl;
+}
---
+++ 20(1) Классы и объекты/fraction.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+struct fraction
+{
+	int first;
+	double second;
+	void init(int, double);
+	void show();
+	void read();
+	double cost();
+};
