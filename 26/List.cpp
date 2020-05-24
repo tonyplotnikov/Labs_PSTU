fc3166f711aad4e6f6566bd0c354b6c143dba0ae
tonyplotnikov <63773654+tonyplotnikov@users.noreply.github.com>
2020-05-25 01:23:23 +0500

    Add files via upload

---
+++ 26/List.cpp
@@ -0,0 +1,82 @@
+﻿#include "List.h"
+List::List(int s)
+{
+	if (s > MAX_SIZE) throw 1;
+	size = s;
+	beg = new int[s];
+	for (int i = 0; i < size; i++)
+		beg[i] = 0;
+}
+List::List(const List& v)
+{
+	size = v.size;
+	beg = new int[size];
+	for (int i = 0; i < size; i++)
+		beg[i] = v.beg[i];
+}
+List::~List()
+{
+	if (beg != 0) delete[]beg;
+}
+List::List(int s, int* mas)
+{
+	if (s > MAX_SIZE) throw 1;
+	size = s;
+	beg = new int[size];
+	for (int i = 0; i < size; i++)
+		beg[i] = mas[i];
+}
+const List& List::operator =(const List& v)
+{
+	if (this == &v)return *this;
+	if (beg != 0) delete[]beg;
+	size = v.size;
+	beg = new int[size];
+	for (int i = 0; i < size; i++)
+		beg[i] = v.beg[i];
+	return*this;
+}
+ostream& operator<<(ostream& out, const List& v)
+{
+	if (v.size == 0) out << "Empty\n";
+	else
+	{
+		for (int i = 0; i < v.size; i++)
+			out << v.beg[i] << " ";
+		out << endl;
+	}
+	return out;
+}
+istream& operator >>(istream& in, List& v)
+{
+	for (int i = 0; i < v.size; i++)
+	{
+		cout << ">";
+		in >> v.beg[i];
+	}
+	return in;
+}
+int List::operator ()()
+{
+	return size;
+}
+int List::operator [](int i)
+{
+	if (i < 0)throw 2;
+	if (i >= size) throw 3;
+	return beg[i];
+}
+List List::operator+(const int k)
+{
+	List temp(size);
+	for (int i = 0; i < size; ++i)
+		temp.beg[i] += beg[i] + k;
+	return temp;
+}
+List List::operator ++(int a)
+{
+	if (size + 1 > MAX_SIZE) throw 4;
+	List temp(size + 1, beg);
+	temp.beg[size] = a;
+	return temp;
+}
---
+++ 26/List.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+const int MAX_SIZE = 2;
+class List
+{
+	int size;
+	int* beg;
+public:
+	List() { size = 0; beg = 0; }
+	List(int s);
+	List(int s, int* mas);
+	List(const List& v);
+	~List();
+	const List& operator=(const List& v);
+	int operator()();
+	int operator[](int i);
+	List operator++(int a);
+	List operator+(const int k);
+	friend ostream& operator<<(ostream& out, const List& v);
+	friend istream& operator>>(istream& in, List& v);
+};
